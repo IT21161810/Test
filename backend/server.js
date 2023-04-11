edae6f52414fde184da6237333463ca63922b1a3
Vikum Viraj <Mvikumviraj@gmail.com>
2023-04-11 17:57:02 +0530

    remove url

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,10 @@ cors = require("cors");
 bodyParser = require("body-parser");
 dbConfig = require('./db/database');
 
-/*mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://Viraj:S2yYltHLyRFntqLj@cluster0.ckl7hpd.mongodb.net/student_app?retryWrites=true&w=majority").then(()=>{
+mongoose.Promise = global.Promise;
+mongoose.connect().then(()=>{
 
      app.listen(5000);
      console.log("Connected to Database");
 
-}).catch((err) => console.log(err));*/
+}).catch((err) => console.log(err));
